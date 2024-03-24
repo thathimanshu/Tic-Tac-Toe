f242cdd5fe699b3e3361e22d5d6c1c51f38f4a71
thathimanshu <thathimanshusaini@gmail.com>
2024-03-24 09:41:06 +0530

    added only 3 feature

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#turn");
-/* let x = [];
-let o = []; */
+let turns = [[],[]]; //[x] [o]
 const winCondition = [
     [0,1,2],
     [3,4,5],
@@ -15,6 +14,7 @@ const winCondition = [
 const options = new Array(9).fill("-1");
 let idx = 0;
 let currPlayer = 1;
+const running = false;
 initialize();
 
 function initialize(){
@@ -24,6 +24,13 @@ function initialize(){
     for(let cell of cells){
         cell.addEventListener("click",cellClick);
     }
+    document.querySelector("#zoom").addEventListener("input",(event)=>{
+        for(let cell of cells){
+            let zoomValue = event.target.value;
+            cell.style.height = zoomValue+"rem";
+            cell.style.width = zoomValue+"rem";
+        }
+    });
 }
 function cellClick(){
     const cellIdx = this.getAttribute("cellIndex");
@@ -34,6 +41,7 @@ function cellClick(){
     
 }
 function updateCell(cell,idx){
+    removeLightenCell();
     options[idx] = currPlayer;
     let srcc = currPlayer == 1 ? "x" : "c";  
     let img = document.createElement('img');
@@ -41,13 +49,30 @@ function updateCell(cell,idx){
     img.classList.add("tick");
     cell.appendChild(img);
     checkWinner(idx,currPlayer);
+    turns[currPlayer-1].push(idx);
     currPlayer = currPlayer == 1 ? 2: 1;
+    lightenCell();
+}
+function lightenCell(){
+    if(turns[currPlayer-1].length==3){
+        cells[turns[currPlayer-1][0]].classList.add("lighten");
+    }
+}
+function removeLightenCell(){
+    let idx = currPlayer-1;
+    if(turns[idx].length!=3){
+        return;
+    }
+    cells[turns[idx][0]].classList.remove("lighten");
+    options[turns[idx][0]]=-1;
+    cells[turns[idx][0]].innerHTML= "";
+    turns[idx].splice(0, 1);
+    
 }
 
 function checkWinner(){
     for(let check of winCondition){
         if(options[check[0]]!=-1 && options[check[0]]==options[check[1]] && options[check[1]]==options[check[2]]){
-            console.log(check);
             playerWins(check);
         }
     }
@@ -58,9 +83,11 @@ function playerWins(arr) {
         if (arr.includes(cellIndex)) {
             cell.classList.add("win-cell");
         }
+        else{
+            cell.classList.add("lose-cell");
+        }
     }
 }
-
 function restartGame(){
     
 }
